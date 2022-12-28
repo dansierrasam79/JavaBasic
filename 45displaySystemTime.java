a697c1f40c8f0df1270aac38aa979ab993f53dca
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-28 19:32:44 +0530

    Create 45displaySystemTime.java

---
+++ 45displaySystemTime.java
@@ -0,0 +1,11 @@
+/* Write a Java program to display the system time.
+Sample Output:
+Current Date time: Fri Jun 16 14:17:40 IST 2017
+*/
+import java.util.Date;
+public class Solution {
+public static void main(String[] args) {
+Date date = new Date();
+System.out.println(date);
+}
+}
