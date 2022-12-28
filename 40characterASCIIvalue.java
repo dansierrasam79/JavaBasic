98c0e592bc8f7d9fd630e5fe63a86f9ded843962
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-28 19:20:54 +0530

    Create 40characterASCIIvalue.java

---
+++ 40characterASCIIvalue.java
@@ -0,0 +1,17 @@
+/* Write a Java program to print the ascii value of a given character.
+Expected Output
+The ASCII value of Z is :90
+*/
+public class Solution {
+public static void main(String[] args) {
+char chOne = 'a';
+char chTwo = 'b';
+char chThree = 'Z';
+int charASCII1 = chOne;
+int charASCII2 = chTwo;
+int charASCII3 = chThree;
+System.out.println(charASCII1);
+System.out.println(charASCII2);
+System.out.println(charASCII3);
+}
+}
