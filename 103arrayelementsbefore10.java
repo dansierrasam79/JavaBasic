b6c43c7dfe7b9e1658885c9fca9766a40a8ccfb8
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-30 19:50:37 +0530

    Create 103arrayelementsbefore10.java

---
+++ 103arrayelementsbefore10.java
@@ -0,0 +1,21 @@
+/* Write a Java program to create a new array from a given array of integers, new array will
+contain the elements from the given array before the last element value 10.
+*/
+class Main {
+public static void main(String[] args) {
+int[] myArray = new int[] {12,15,17,23,31,45,10,16,11,14,18,19,21};
+int[] myArray2 = new int[6];
+int c = 0;
+for (int i=0; i < myArray.length;i++) {
+if (myArray[i] == 10) {
+for (int j = 0; j < i;j++) {
+myArray2[c] = myArray[j];
+c++;
+}
+}
+}
+for (int k = 0; k < myArray2.length;k++) {
+System.out.println(myArray2[k]);
+}
+}
+}
