230767a0387b075a78be2c6735cf1cc9265e2f35
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-30 20:01:20 +0530

    Create 108formstaircaseshape.java

---
+++ 108formstaircaseshape.java
@@ -0,0 +1,14 @@
+/* Write a Java program to form a staircase shape of n coins where every k-th row must have
+exactly k coins.*/
+import java.util.Scanner;
+class Main {
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+System.out.print("Input a positive integer: ");
+int n = in.nextInt();
+if (n>0) {
+System.out.println("Number of rows: "+((int)((Math.sqrt(8 * (long)n + 1) - 1) / 2)));
+}
+in.close();
+}
+}
