a2ad47b06c96e17b522811ef464f313aa745d27f
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2022-12-30 17:56:13 +0530

    Create 89valueenvvariable.java

---
+++ 89valueenvvariable.java
@@ -0,0 +1,12 @@
+/* Write a Java program to get the value of the environment variable PATH, TEMP,USERNAME.*/
+public class Main {
+public static void main(String[] args) {
+System.out.println(System.getenv("TEMP"));
+System.out.println("");
+System.out.println(System.getenv("PATH"));
+System.out.println("");
+System.out.println(System.getenv("USERNAME"));
+System.out.println("");
+System.out.println(System.getenv("OS"));
+}
+}
